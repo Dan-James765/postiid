af274df1ef860c3e2a802896ead101863799c4a7
Dan James <danjameshh@hotmail.com>
2022-02-05 23:10:21 +0000

    FrontEnd UptoDate B4 Starting BackendX

--- src/components/Header-Folder/Header.tsx
+++ src/components/Header-Folder/Header.tsx
@@ -1,13 +1,17 @@
+import { Fade } from "react-awesome-reveal";
+
 function Header() {
   return (
     <div>
-      <header className="shadow-lg py-6 flex items-center justify-center">
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/768px-Instagram_logo_2016.svg.png"
-          className="h-8 w-8 max-h-8 max-w-8"
-          alt=""
-        />
-      </header>
+      <Fade>
+        <header className="shadow-lg py-6 flex items-center justify-center">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/768px-Instagram_logo_2016.svg.png"
+            className="h-8 w-8 max-h-8 max-w-8"
+            alt=""
+          />
+        </header>
+      </Fade>
     </div>
   );
 }
