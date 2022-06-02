c840be156f733269291f9609a8e10270c6975be2
Dan James <danjameshh@hotmail.com>
2022-06-02 13:19:58 +0100

    Dating Sidebar Resposviness Completed

--- front-end/src/components/Dating-Folder/DatingMessages.tsx
+++ front-end/src/components/Dating-Folder/DatingMessages.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 function DatingMessages({
   id,
   username,
@@ -14,17 +12,17 @@ function DatingMessages({
   return (
     <>
       <div className="bg-gray-50 border-slate-100 border-[1px] h-auto hover:bg-gray-100 cursor-pointer">
-        <div className="flex items-center gap-x-1 py-1">
+        <div className="flex items-center gap-x-1 py-1 2xl:justify-start xl:justify-start lg:justify-start xs:justify-center">
           <img
             src={userImg}
             alt=""
-            className="cursor-pointer rounded-full h-8 w-8 object-contain border border-gray-300"
+            className="cursor-pointer rounded-full h-8 w-8 object-contain border border-gray-300 "
           />
-          <h1 className="hidden cursor-pointer md:block 2xl:font-normal xl:font-normal lg:font-medium lg:text-xs">
+          <h1 className="hidden cursor-pointer md:block text-xs 2xl:font-normal xl:font-normal lg:font-medium lg:text-xs">
             {username}
           </h1>
         </div>
-        <h1 className="flex-col hidden md:block lg:text-xs text-gray-400 font-normal truncate">
+        <h1 className="flex-col hidden md:block lg:text-xs text-xs text-gray-400 font-normal truncate">
           {message}
         </h1>
       </div>
